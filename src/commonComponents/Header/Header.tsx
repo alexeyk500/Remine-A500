b12df04af23e7996b4f23791a1f3371488e3c243
alexeyk500 <alexeyk500@yandex.ru>
2022-07-14 09:55:03 +0300

    header - done

--- src/commonComponents/Header/Header.tsx
+++ src/commonComponents/Header/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import classes from "./Header.module.css";
+import SearchComponent from "../SearchComponent/SearchComponent";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.title}>ИСУП</div>
+      <div className={classes.rightColumn}>
+        <SearchComponent />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/commonComponents/SearchComponent/SearchComponent.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./SearchComponent.module.css";
+import search from "./../../images/search.svg";
+import bell from "./../../images/bell.svg";
+import query from "./../../images/query.svg";
+import avatar from "./../../images/avatar.svg";
+
+const SearchComponent: React.FC = () => {
+  return (
+    <>
+      <div className={classes.container}>
+        <img src={search} alt="search ico" />
+      </div>
+      <div className={classes.bellContainer}>
+        <img src={bell} alt="bell ico" />
+      </div>
+      <div className={classes.queryContainer}>
+        <img src={query} alt="query ico" />
+      </div>
+      <div className={classes.avatarContainer}>
+        <img src={avatar} alt="avatar ico" />
+      </div>
+    </>
+  );
+};
+
+export default SearchComponent;
