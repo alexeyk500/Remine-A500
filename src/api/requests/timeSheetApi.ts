6e5821ee76bde7089cb74f53932cd2c2b09e8871
alexeyk500 <alexeyk500@yandex.ru>
2022-07-19 11:01:58 +0300

    heroku cors new - fixed

--- src/api/requests/timeSheetApi.ts
+++ src/api/requests/timeSheetApi.ts
@@ -1,10 +1,10 @@
-import { instanceAxios } from "../instanceAxios";
 import { TimeEntryType } from "../../types/types";
+import axios from "axios";
 
 export const timeSheetApi = {
   async fetchTimeEntries({ from, to }: { from: string; to: string }) {
-    const response = await instanceAxios.get<{ time_entries: TimeEntryType[] }>(
-      `time_entries.json?key=${process.env.REACT_APP_REDMINE_KEY}&from=${from}&to=${to}`
+    const response = await axios.get<{ time_entries: TimeEntryType[] }>(
+      `https://cors-anywhere.herokuapp.com/https://opt2.rbtechnologies.ru:9909/time_entries.json?key=${process.env.REACT_APP_REDMINE_KEY}&from=${from}&to=${to}`
     );
     return response.data;
   },
